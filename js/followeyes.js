9f64eb7da577a95f06b0324e1d307fbfbf525b31
Ivan Ryabov <chiba.link@gmail.com>
2014-05-01 04:57:02 +0400

    v0.5

--- js/followeyes.js
+++ js/followeyes.js
@@ -1,34 +1,76 @@
 /*
  * Followeyes.js.
  * Created by Chiba.
+ *
+ * TODO: {
+ *      1: different radius for different eyes
+ * }
  */
 
-jQuery(document).ready(function() {
-    // Eye initialization
-    var eyeBall = $('.eye-ball'),
-        eyePupil = eyeBall.find('.eye-pupil'),
-        eyeCenterX = eyeBall.offset().left + 16,
-        eyeCenterY = eyeBall.offset().top + 16,
-        r = 12,                                      // gyration radius
-        pupilX, pupilY,                             // pupil coordinates
-        cursorX, cursorY,                           // cursor coordinates
+
+(function($) {
+    $.fn.followeyes = function(params) {
+        // Define
+        var eyeBall, eyePupil,
+            eyeCenterX, eyeCenterY,
+            pupilX, pupilY,
+            cursorX, cursorY,
+            r, isProcessed,
+            $eyes = {},
+            i = 0, n = 0;
+
+        // Eyes initialization
+        if (!params['eyeBall'])
+            eyeBall = $('.eye-ball');
+        else
+            eyeBall = $('.' + params['eyeBall']);
+
+        if (!params['eyePupil'])
+            eyePupil = eyeBall.find('.eye-pupil');
+        else
+            eyePupil = eyeBall.find('.' + params['eyePupil']);
+
+        if (!params['radius'])
+            r = 10;
+        else
+            r = params['radius'];
+
         isProcessed = false;
 
-    $('html,body').mousemove(function(event) {
-        if (!isProcessed) {
-            isProcessed = true;
+        // Obtain the coordinates of the centers of all eyes
+        i = 1;
+        eyeBall.each(function() {
+            eyeCenterX = $(this).offset().left + ($(this).width() / 2);
+            eyeCenterY = $(this).offset().top + ($(this).height() / 2);
+            $eyes[i] = {
+                eyeCenterX: eyeCenterX,
+                eyeCenterY: eyeCenterY,
+            }
+            i++;
+        });
 
-            cursorX = event.pageX
-            cursorY = event.pageY;
+        // Rolling eyes when moving the mouse
+        $(document).on('mousemove', 'html,body', function(event) {
+            if (!isProcessed) {
+                isProcessed = true;
+                
+                n = 1;
+                eyePupil.each(function() {
+                    cursorX = event.pageX
+                    cursorY = event.pageY;
 
-            pupilY = ((r * (cursorY - eyeCenterY)) / Math.sqrt((cursorX - eyeCenterX) * (cursorX - eyeCenterX) + (cursorY - eyeCenterY) * (cursorY - eyeCenterY))) + eyeCenterY;
-            pupilX = (((pupilY - eyeCenterY) * (cursorX - eyeCenterX)) / (cursorY - eyeCenterY)) + eyeCenterX;
+                    pupilY = ((r * (cursorY - $eyes[n].eyeCenterY)) / Math.sqrt((cursorX - $eyes[n].eyeCenterX) * (cursorX - $eyes[n].eyeCenterX) + (cursorY - $eyes[n].eyeCenterY) * (cursorY - $eyes[n].eyeCenterY))) + $eyes[n].eyeCenterY;
+                    pupilX = (((pupilY - $eyes[n].eyeCenterY) * (cursorX - $eyes[n].eyeCenterX)) / (cursorY - $eyes[n].eyeCenterY)) + $eyes[n].eyeCenterX;
 
-            eyePupil.css({
-                marginTop: (pupilY - eyeCenterY) + 'px',
-                marginLeft: (pupilX - eyeCenterX) * 1 + 'px'
-            });
-            isProcessed = false;
-        }
-    });
-});
+                    $(this).css({
+                        marginTop: (pupilY - $eyes[n].eyeCenterY) + 'px',
+                        marginLeft: (pupilX - $eyes[n].eyeCenterX) * 1 + 'px'
+                    });
+                    n++;
+                });
+
+                isProcessed = false;
+            }
+        });
+    };
+}) (jQuery);
